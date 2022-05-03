94b945092292ddb9d7f2af163597d1d93d91602e
Mykhailo Seleznov <mseleznov@zoolatech.com>
2022-05-03 11:53:33 +0300

    Integrated BoardView to project

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -9,9 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var boardView: BoardView!
+
+    var viewState = BoardView.State(cells: [[BoardView.CellState(text: "", color: .black),BoardView.CellState(text: "", color: .black),BoardView.CellState(text: "", color: .black)],
+                                            [BoardView.CellState(text: "", color: .black),BoardView.CellState(text: "", color: .black),BoardView.CellState(text: "", color: .black)],
+                                            [BoardView.CellState(text: "", color: .black),BoardView.CellState(text: "", color: .black),BoardView.CellState(text: "", color: .black)]])
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        boardView.delegate = self
+
+        boardView.applyViewState(state: viewState)
+    }
+
+
+}
+
+extension ViewController: BoardViewDelegate {
+    func didSelectCell(at indexPath: IndexPath) {
+        var cells = viewState.cells
+
+        cells[indexPath.section][indexPath.row] = .init(text: "X", color: .green)
+
+        viewState = .init(cells: cells)
+
+        boardView.applyViewState(state: viewState)
     }
 
 
